46f2178249164a99031ec8ed89da6137510f66d7
sakethbhardwajV <sakethb.vangipuram@gmail.com>
2023-10-15 17:26:39 +0530

    Create User Model & Post Model

---
+++ backend/models/postModel.js
@@ -0,0 +1,40 @@
+// user, content, likedBy, bookmarkedBy, superLikedBy, parentPost
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  likedBy: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  bookmarkedBy: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  superLikedBy: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  parentPost: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post",
+  },
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
