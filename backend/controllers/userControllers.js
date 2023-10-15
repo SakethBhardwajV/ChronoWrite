d3f9381cdc87cdae4792e62519467f249fedfc7f
sakethbhardwajV <sakethb.vangipuram@gmail.com>
2023-10-16 00:25:35 +0530

    Update User Profile

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -99,7 +99,29 @@ const getUserProfile = asyncHandler(async (req, res) => {
 // @route   PUT /api/users/profile
 // @access  Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.json("update user profile");
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.username = req.body.username || user.username;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      username: updatedUser.username,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 // @desc    Delete user profile
