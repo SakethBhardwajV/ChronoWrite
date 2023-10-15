d01d2d6830cae700c7d406e1601ad67240c8ebf3
sakethbhardwajV <sakethb.vangipuram@gmail.com>
2023-10-16 00:40:56 +0530

    Get User By ID - Admin

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -162,7 +162,14 @@ const getUsers = asyncHandler(async (req, res) => {
 // @route   GET /api/users/:id
 // @access  Private/Admin
 const getUserByID = asyncHandler(async (req, res) => {
-  res.json("get user by id");
+  const user = await User.findById(req.params.id).select("-password");
+
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 // @desc    Delete user by id
