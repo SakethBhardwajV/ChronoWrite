4168873b8b002fca001e92482e5502b71d6e34d1
sakethbhardwajV <sakethb.vangipuram@gmail.com>
2023-11-06 19:17:00 +0530

    Func. Post Component: Like, Bookmark, SuperLike

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,12 +1,91 @@
 import { useState } from "react";
 import styles from "../styles/Post.module.css";
+import {
+  useLikePostMutation,
+  useUnlikePostMutation,
+  useBookmarkPostMutation,
+  useUnbookmarkPostMutation,
+  useSuperLikePostMutation,
+  useUnSuperLikePostMutation,
+} from "../slices/postApiSlice";
 
 const Post = ({ content, details, stats }) => {
   const { bookmarkedBy, likedBy, superLikedBy } = stats;
 
+  const [likeCount, setLikeCount] = useState(likedBy.length);
+  const [bookmarkCount, setBookmarkCount] = useState(bookmarkedBy.length);
+  const [superLiked, setSuperLiked] = useState(superLikedBy.length);
+
   const [isLiked, setIsLiked] = useState(false);
   const [isSuperLiked, setIsSuperLiked] = useState(false);
   const [isBookmarked, setIsBookmarked] = useState(false);
+
+  const [likePost] = useLikePostMutation();
+  const [unlikePost] = useUnlikePostMutation();
+  const [bookmarkPost] = useBookmarkPostMutation();
+  const [unbookmarkPost] = useUnbookmarkPostMutation();
+  const [superLikePost] = useSuperLikePostMutation();
+  const [unSuperLikePost] = useUnSuperLikePostMutation();
+
+  const handleLike = async () => {
+    try {
+      await likePost(stats._id);
+      setLikeCount(likeCount + 1);
+      setIsLiked(!isLiked);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleUnlike = async () => {
+    try {
+      await unlikePost(stats._id);
+      setLikeCount(likeCount - 1);
+      setIsLiked(!isLiked);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleBookmark = async () => {
+    try {
+      await bookmarkPost(stats._id);
+      setBookmarkCount(bookmarkCount + 1);
+      setIsBookmarked(!isBookmarked);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleUnbookmark = async () => {
+    try {
+      await unbookmarkPost(stats._id);
+      setBookmarkCount(bookmarkCount - 1);
+      setIsBookmarked(!isBookmarked);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleSuperLike = async () => {
+    try {
+      await superLikePost(stats._id);
+      setSuperLiked(superLiked + 1);
+      setIsSuperLiked(!isSuperLiked);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleUnSuperLike = async () => {
+    try {
+      await unSuperLikePost(stats._id);
+      setSuperLiked(superLiked - 1);
+      setIsSuperLiked(!isSuperLiked);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className={styles["post"]}>
@@ -31,7 +110,7 @@ const Post = ({ content, details, stats }) => {
             className={`${styles["post__button"]} ${
               styles["post__button--like"]
             } ${isLiked ? styles["post__button--active"] : ""}`}
-            onClick={() => setIsLiked(!isLiked)}
+            onClick={!isLiked ? handleLike : handleUnlike}
           >
             <svg
               width="24"
@@ -44,7 +123,7 @@ const Post = ({ content, details, stats }) => {
               <path d="M3.66276 13.2135L9.82378 19.7065C11.0068 20.9532 12.9933 20.9532 14.1762 19.7065L20.3373 13.2135C22.5543 10.877 22.5543 7.08882 20.3373 4.75235C18.1203 2.41588 14.5258 2.41588 12.3088 4.75235V4.75235C12.1409 4.92925 11.8591 4.92925 11.6912 4.75235V4.75235C9.47422 2.41588 5.87976 2.41588 3.66276 4.75235C1.44576 7.08883 1.44576 10.877 3.66276 13.2135Z" />
             </svg>
             <span className={`${styles["post__button__count"]}`}>
-              {likedBy.length}
+              {likeCount}
             </span>
           </button>
           <button
@@ -77,7 +156,7 @@ const Post = ({ content, details, stats }) => {
             className={`${styles["post__button"]} ${
               styles["post__button--bookmark"]
             }  ${isBookmarked ? styles["post__button--active"] : ""}`}
-            onClick={() => setIsBookmarked(!isBookmarked)}
+            onClick={!isBookmarked ? handleBookmark : handleUnbookmark}
           >
             <svg
               width="24"
@@ -91,14 +170,14 @@ const Post = ({ content, details, stats }) => {
               <path d="M5 7C5 4.79086 6.79086 3 9 3H15C17.2091 3 19 4.79086 19 7V20.1683C19 20.9595 18.1248 21.4373 17.4592 21.0095L13.0815 18.1953C12.4227 17.7717 11.5773 17.7717 10.9185 18.1953L6.54076 21.0095C5.87525 21.4373 5 20.9595 5 20.1683V7Z" />
             </svg>
             <span className={styles["post__button__count"]}>
-              {bookmarkedBy.length}
+              {bookmarkCount}
             </span>
           </button>
           <button
             className={`${styles["post__button"]} ${
               styles["post__button--superlike"]
             } ${isSuperLiked ? styles["post__button--active"] : ""}`}
-            onClick={() => setIsSuperLiked(!isSuperLiked)}
+            onClick={!isSuperLiked ? handleSuperLike : handleUnSuperLike}
           >
             <svg
               width="24"
@@ -120,9 +199,7 @@ const Post = ({ content, details, stats }) => {
                 </clipPath>
               </defs>
             </svg>
-            <span className={styles["post__button__count"]}>
-              {superLikedBy.length}
-            </span>
+            <span className={styles["post__button__count"]}>{superLiked}</span>
           </button>
           <div className={styles["post__share-container"]}>
             <button
--- frontend/src/slices/postApiSlice.js
+++ frontend/src/slices/postApiSlice.js
@@ -17,8 +17,52 @@ export const postApiSlice = apiSlice.injectEndpoints({
       }),
       keepUnusedDataFor: 5,
     }),
+    likePost: builder.mutation({
+      query: (postID) => ({
+        url: `${POSTS_URL}/like/${postID}`,
+        method: "PUT",
+      }),
+    }),
+    unlikePost: builder.mutation({
+      query: (postID) => ({
+        url: `${POSTS_URL}/unlike/${postID}`,
+        method: "PUT",
+      }),
+    }),
+    bookmarkPost: builder.mutation({
+      query: (postID) => ({
+        url: `${POSTS_URL}/bookmark/${postID}`,
+        method: "PUT",
+      }),
+    }),
+    unbookmarkPost: builder.mutation({
+      query: (postID) => ({
+        url: `${POSTS_URL}/unbookmark/${postID}`,
+        method: "PUT",
+      }),
+    }),
+    superLikePost: builder.mutation({
+      query: (postID) => ({
+        url: `${POSTS_URL}/superlike/${postID}`,
+        method: "PUT",
+      }),
+    }),
+    unSuperLikePost: builder.mutation({
+      query: (postID) => ({
+        url: `${POSTS_URL}/unsuperlike/${postID}`,
+        method: "PUT",
+      }),
+    }),
   }),
 });
 
-export const { useCreatePostMutation, useGetFollowingUsersPostsQuery } =
-  postApiSlice;
+export const {
+  useCreatePostMutation,
+  useGetFollowingUsersPostsQuery,
+  useLikePostMutation,
+  useUnlikePostMutation,
+  useBookmarkPostMutation,
+  useUnbookmarkPostMutation,
+  useSuperLikePostMutation,
+  useUnSuperLikePostMutation,
+} = postApiSlice;
